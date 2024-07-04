c35ead195f2b6baaddecf137d90ddd727f47b692
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-07-03 23:16:22 -0500

    added links to the projects github and demo, added back information to the cards

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -13,14 +13,34 @@ const Projects = ({ language }) => {
           <div className="card">
             <div className="front">
               <h1>Inventech</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus dignissimos cupiditate nobis 
-                nesciunt eaque, quam perspiciatis cumque deserunt 
-                libero quo. Quae dolores delectus aut saepe neque
-                 asperiores iure dolore fugiat!</p>
+              <p className="info-pro">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus
+                dignissimos cupiditate nobis nesciunt eaque, quam perspiciatis
+                cumque deserunt libero quo. Quae dolores delectus aut saepe
+                neque asperiores iure dolore fugiat!
+              </p>
             </div>
             <div className="back">
-              <h1>Technologies</h1>
-              <p>Roles</p>
+              <p className="titles">Technologies:</p>
+              <img src="/tech/angular.png" alt="" className="img-links" />
+              <img src="/tech/ts.png" alt="" className="img-links" />
+              <img src="/tech/python.png" alt="" className="img-links" />
+              <p className="titles">
+                Roles: <p className="info">Scrum Master, Front-end Developer</p>
+              </p>
+              <p className="titles">Interesting Links: </p>
+              <a
+                href="https://inventechcol.netlify.app/dashboard"
+                target="_blank"
+              >
+                <img src="/img/link.png" alt="GitHub" className="img-links" />
+              </a>
+              <a
+                href="https://github.com/Unisoft-Project/Front-Unisoft"
+                target="_blank"
+              >
+                <img src="/img/github.png" alt="" className="img-links" />
+              </a>
             </div>
           </div>
         </div>
@@ -28,14 +48,34 @@ const Projects = ({ language }) => {
           <div className="card">
             <div className="front">
               <h1>Alice Effect</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus dignissimos cupiditate nobis 
-                nesciunt eaque, quam perspiciatis cumque deserunt 
-                libero quo. Quae dolores delectus aut saepe neque
-                 asperiores iure dolore fugiat!</p>
+              <p className="info-pro">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus
+                dignissimos cupiditate nobis nesciunt eaque, quam perspiciatis
+                cumque deserunt libero quo. Quae dolores delectus aut saepe
+                neque asperiores iure dolore fugiat!
+              </p>
             </div>
             <div className="back">
-              <h1>Technologies</h1>
-              <p>Roles</p>
+              <p className="titles">Technologies:</p>
+              <img src="/tech/react.png" alt="" className="img-links" />
+              <img src="/tech/js.png" alt="" className="img-links" />
+              <img src="/tech/nodejs.png" alt="" className="img-links" />
+              <p className="titles">
+                Roles: <p className="info">Scrum Master, Front-end Developer</p>
+              </p>
+              <p className="titles">Interesting Links: </p>
+              <a
+                href="https://el-efecto-alice.vercel.app"
+                target="_blank"
+              >
+                <img src="/img/link.png" alt="GitHub" className="img-links" />
+              </a>
+              <a
+                href="https://github.com/valentinabarbetty19/El-Efecto-Alice"
+                target="_blank"
+              >
+                <img src="/img/github.png" alt="" className="img-links" />
+              </a>
             </div>
           </div>
         </div>
@@ -43,14 +83,34 @@ const Projects = ({ language }) => {
           <div className="card">
             <div className="front">
               <h1>Softdipal</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus dignissimos cupiditate nobis 
-                nesciunt eaque, quam perspiciatis cumque deserunt 
-                libero quo. Quae dolores delectus aut saepe neque
-                 asperiores iure dolore fugiat!</p>
+              <p className="info-pro">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus
+                dignissimos cupiditate nobis nesciunt eaque, quam perspiciatis
+                cumque deserunt libero quo. Quae dolores delectus aut saepe
+                neque asperiores iure dolore fugiat!
+              </p>
             </div>
             <div className="back">
-              <h1>Technologies</h1>
-              <p>Roles</p>
+              <p className="titles">Technologies:</p>
+              <img src="/tech/angular.png" alt="" className="img-links" />
+              <img src="/tech/ts.png" alt="" className="img-links" />
+              <img src="/tech/python.png" alt="" className="img-links" />
+              <p className="titles">
+                Roles: <p className="info">Scrum Master, Full-Stack Developer</p>
+              </p>
+              <p className="titles">Interesting Links: </p>
+              <a
+                href="https://inventechcol.netlify.app/dashboard"
+                target="_blank"
+              >
+                <img src="/img/link.png" alt="GitHub" className="img-links" />
+              </a>
+              <a
+                href="https://github.com/Unisoft-Project/Front-Unisoft"
+                target="_blank"
+              >
+                <img src="/img/github.png" alt="" className="img-links" />
+              </a>
             </div>
           </div>
         </div>
